e4fc0d3755b8ddcc92fdfd4acf22765f6c858895
gab <perinogabriel1@gmail.com>
2024-11-27 20:40:38 +0100

    feat : main - Improve loop game design - Remove print useless - Remove useless function

--- base_game/classes/Character.py
+++ base_game/classes/Character.py
@@ -7,26 +7,15 @@ class Character:
 
 
     def attack(self, target):
-        print(f"{self.name} attacks {target.name}!")
+        print(f"{self.name} attacks {target.name} and deals {self.strength} damages!")
         target.take_damage(self.strength)
-    def defend(self, enemy):
-        enemy.attack()
-        self.health -= enemy.strength
-        print(f"{self.name} defends!")
-
 
     def health_check(self):
         print(f"{self.name} has {self.health} health left")
 
-    def die(self):
-        print(f"{self.name} has died!")
 
     def take_damage(self, damage):
         self.health -= damage
-        if self.health <= 0:
-            self.die()
-        else:
-            print(f"{self.name} takes {damage} damage!")
 
     def reload_health(self):
         self.health = self.max_health
--- base_game/main.py
+++ base_game/main.py
@@ -1,3 +1,5 @@
+from click import pause
+
 from classes.Boss import Boss
 from classes.Castle import Castle
 from classes.Enemy import Enemy
@@ -6,9 +8,8 @@ from classes.Player import Player
 def choice_action(player):
     choice = 0
     while choice not in ["1", "2"]:
-        print("What do you want to do?")
-        print("1. Attack")
-        print("2. Heal")
+        print("---OPTIONS-----\n"
+              "1. Attack    2. Heal")
         choice = input()
     return int(choice)
 
@@ -18,37 +19,39 @@ def display_ascii_art(file, reduced=False, scale=0.5):
 
 
 def define_character():
-    player = Player("Link", 60, 120, 15)
+    player = Player("Link", 60, 60, 15)
     enemy = Enemy("Bokoblin", 30, 30, 5)
     boss = Boss("Ganon", 150, 150, 20)
     return player, enemy, boss
 
 
 def main():
-    hyrule = Castle(3)
+    hyrule = Castle(10)
     boss_level = hyrule.nb_level
     # display_ascii_art("Title.txt")
     player, enemy, boss = define_character()
     for i in range(0, boss_level):
         if i == boss_level:
             enemy = boss
+            print("You have reached the boss level!")
         enemy.reload_health()
-        print(f"Level {i + 1}")
+        print(f"========== FIGHT {i + 1} ==========")
         while enemy.health > 0 and player.health > 0:
-            print("You encounter an enemy!")
-            print(f"{enemy.name} has {enemy.health} health.")
-            print(f"{player.name} has {player.health} health.")
+            print(f"{enemy.name} "
+                  f"HP: {enemy.health}/{enemy.max_health}")
+            print(f"{player.name} "
+                  f"HP: {player.health}/{player.max_health}")
             choice = choice_action(player)
+            print(f"You encounter {enemy.name}!")
             if choice == 1:
                 player.attack(enemy)
             else:
                 player.self_heal()
             if enemy.health <= 0:
+                print(f"{enemy.name} died!")
+                pause("Press any key to continue...")
                 i += 1
-                if i < boss_level:
-                    print("You have reached the next level!")
-                else:
-
+                if i == boss_level:
                     print("You defeated the boss!")
                     display_ascii_art("victory.txt")
                     return
