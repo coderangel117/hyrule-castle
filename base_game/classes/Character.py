37ae33a47a197b6ecad1ac366ce457cc0fc2685a
Gabriel PERINO <perinogabriel1@gmail.com>
2026-02-26 13:49:58 +0100

    refactor(characters) : rename property - Adapt properties' class from json

--- base_game/classes/Character.py
+++ base_game/classes/Character.py
@@ -1,20 +1,22 @@
 class Character:
-    def __init__(self, name, health, max_health, strength, defense):
+    def __init__(self, name, hp, str_, def_, spd, luck):
         self.name = name
-        self.health = health
-        self.max_health = max_health
-        self.strength = strength
-        self.defense = defense
+        self.hp = hp
+        self.max_health = hp
+        self.strength = str_
+        self.defense = def_
+        self.speed = spd
+        self.luck = luck
 
     def attack(self, target):
         target.take_damage(self.strength)
 
     def health_check(self):
-        print("{} a {} points de vie restants".format(self.name, self.health))
+        print("{} a {} points de vie restants".format(self.name, self.hp))
 
     def take_damage(self, damage):
-        final_damage = damage - self.defense
-        self.health -= final_damage
+        final_damage = damage - self.def_
+        self.hp -= final_damage
 
     def reload_health(self):
-        self.health = self.max_health
+        self.hp = self.max_health
--- base_game/classes/Player.py
+++ base_game/classes/Player.py
@@ -2,8 +2,8 @@ from .Character import Character
 
 
 class Player(Character):
-    def __init__(self, name, health, max_health, strength, defense):
-        super().__init__(name, health, max_health, strength, defense)
+    def __init__(self, name, hp, str_, def_, spd, luck):
+        super().__init__(name, hp, str_, def_, spd, luck)
 
     def self_heal(self):
         """
@@ -11,9 +11,9 @@ class Player(Character):
         Heal permit to recovers half max point
         """
         hp = self.max_health / 4
-        if self.health + hp > self.max_health:
-            self.health = self.max_health
-            int(self.health)
+        if self.hp + hp > self.max_health:
+            self.hp = self.max_health
+            int(self.hp)
         else:
-            self.health += hp
-            int(self.health)
+            self.hp += hp
+            int(self.hp)
